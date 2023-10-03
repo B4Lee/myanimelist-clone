212716b5578d9a231e7012585e9030f1e663cf8c
B4Lee <iqbalanggoro48@gmail.com>
2023-10-03 16:39:38 +0700

    Create SlotComponent.vue

---
+++ src/components/SlotComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- SLOT: POSTER & IMAGE -->
+    <div class="row-span-full col-span-1 mr-3 ml-3 border-r-2">
+      <slot name="poster"></slot>
+    </div>
+
+    <!-- SLOT: CONTENT -->                                          
+    <div class="col-span-3 pt-12">
+      <slot name="content"></slot>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
